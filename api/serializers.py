1ec6a86a99ef5f02b411aba77dccb9d2d64996c2
Owi-ti <owittiesther@gmail.com>
2022-10-10 22:19:46 +0300

    Create serializers, views and routes for models

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,45 @@
 from rest_framework import serializers
-from wallet.models import Customer
+from wallet.models import Customer, Wallet, Account, Card, Transaction, Loan, Recipient, Notification
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model= Customer
-        fields= ("first_name","email","age","adress")
+        fields= ("first_name","email","age","address")
+
+class WalletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wallet
+        fields = ("balance", "customer", "status", "amount", "time", "currency")
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ("account_number", "account_type","account_name", "account_balance","wallet")        
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = ("card_number", "card_name","date_issued", "card_type","expiry_date","security_code")        
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ("transaction_ref", "wallet","transaction_amount", "transaction_name","transaction_type","transaction_charge","date_and_time","original_account","destination_account","receipt")        
+
+
+class LoanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Loan
+        fields = ("loan_type", "amount","transaction", "interest_rate","wallet","loan_balance","loan_term","guarantor","status","payment_due_date") 
+
+
+class RecipientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipient
+        fields = ("receipt", "date_time","bill_number", "amount","receipt_file") 
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = ("message", "title","recipient", "date_time","status")         
--- api/views.py
+++ api/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from wallet.models import Customer
-from .serializers import CustomerSerializer
+from wallet.models import Customer, Wallet, Account, Card, Transaction, Loan, Recipient,Notification
+from .serializers import CustomerSerializer, WalletSerializer, AccountSerializer, CardSerializer, TransactionSerializer, LoanSerializer, RecipientSerializer, NotificationSerializer
 # Create your views here.
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset= Customer.objects.all()
     serializer_class = CustomerSerializer
-    
+
+class WalletViewSet(viewsets.ModelViewSet):
+    queryset =  Wallet.objects.all()
+    serializer_class = WalletSerializer
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset= Account.objects.all()
+    serializer_class = AccountSerializer
+
+class CardViewSet(viewsets.ModelViewSet):
+    queryset= Card.objects.all()
+    serializer_class = CardSerializer    
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset= Transaction.objects.all()
+    serializer_class = TransactionSerializer    
+
+
+class LoanViewSet(viewsets.ModelViewSet):
+    queryset= Loan.objects.all()
+    serializer_class = LoanSerializer    
+
+
+class RecipientViewSet(viewsets.ModelViewSet):
+    queryset= Recipient.objects.all()
+    serializer_class = RecipientSerializer      
+
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset= Notification.objects.all()
+    serializer_class = NotificationSerializer   
